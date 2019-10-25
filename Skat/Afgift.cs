99e6233f88ab079f47388e87c297fc5dc2b59c7e
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2019-10-25 13:05:50 +0200

    Add project files.

---
+++ Skat/Afgift.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Skat
+{
+    public class Afgift
+    {
+        // En fejlbesked man får hvis 
+        public const string CarPriceIsMinusOrNull = "Bilafgiften kan ikke beregnes, da værdien er enten nul eller minus. Indsæt et positiv tal.";
+
+        /// <summary>
+        /// Metoden beregner afgiften på en almindelig bil. Metoden tager parameterværdien pris
+        /// og udregne afgiften ved hjælp af formlerne:
+        ///      (pris x 0.85) for biler med værdi under 200.000 kr.
+        ///      ((pris X 0.85)-130.000) for biler med værdi over 200.000 kr.
+        /// Returværdien er afgiften på bilen som en int.
+        /// </summary>
+        /// <param name="pris"></param>
+        /// <returns></returns>
+        /// 
+        public static int BilAfgift(int pris)
+        {
+            if (pris >= 0)
+            {
+                double result = 0;
+                if (pris <= 200000)
+                {
+                    result = (pris * 0.85);
+                }
+
+                if (pris > 200000)
+                {
+                    result = (pris * 1.5) - 130000;
+                }
+                return Convert.ToInt32(result);
+            }
+            throw new System.ArgumentOutOfRangeException("pris", pris, CarPriceIsMinusOrNull);
+
+        }
+        /// <summary>
+        /// Metoden beregner afgiften på en elektrisk bil. Metoden tager parameterværdien pris
+        /// og udregne afgiften ved hjælp af formlen:
+        ///      (BilAfgift * 0.2)
+        /// Returværdien er afgiften på elbilen som en int.
+        /// </summary>
+        /// <param name="pris"></param>
+        /// <returns></returns>
+        public static int ElBilAfgift(int pris)
+        {
+            return Convert.ToInt32(BilAfgift(pris) * 0.2);
+
+        }
+
+    }
+}
---
+++ TestBilAfgift/UnitTest1.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestBilAfgift
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Unit Test car price
+        [TestMethod]
+        public void CarPrice_WhenPriceIsNegative_ThrowArgumentOutOfRange()
+        {
+            // Arrange
+            int bil = -200000;
+
+            // Act
+            try
+            {
+                Skat.Afgift.BilAfgift(bil);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, Skat.Afgift.CarPriceIsMinusOrNull);
+            }
+        }
+
+        // Unit Test car tax
+        [TestMethod]
+        public void CarTax_CarPriceUnderTwoThousand_TestSucced()
+        {
+            // Arrange
+            int bil = 199999;
+            int expectedResult = Convert.ToInt32(bil * 0.85);
+
+            // Act
+            int result = Skat.Afgift.BilAfgift(bil);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void CarTax_CarPriceIsTwoThousand_TestSucced()
+        {
+            // Arrange
+            int bil4 = 200000;
+            int expectedResult = Convert.ToInt32(bil4 * 0.85);
+
+            // Act
+            int result = Skat.Afgift.BilAfgift(bil4);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void CarTax_CarPriceOverTwoThousand_TestSucced()
+        {
+            // Arrange
+            int bil5 = 250000;
+            int expectedResult = Convert.ToInt32((bil5 * 1.5) - 130000);
+
+            // Act
+            int result = Skat.Afgift.BilAfgift(bil5);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        // Unit Test elcar price
+
+        [TestMethod]
+        public void ElCarPrice_ElCarPriceIsNegative_ThrowArgumentOutOfRange()
+        {
+            // Arrange
+            int bil = -150000;
+
+            // Act
+            try
+            {
+                Skat.Afgift.ElBilAfgift(bil);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, Skat.Afgift.CarPriceIsMinusOrNull);
+            }
+
+        }
+
+        // Unit Test elcar tax
+        [TestMethod]
+        public void ElCarTax_CarPriceUnderTwoHundredThousand_TestSucced()
+        {
+            // Arrange
+            int bil3 = 199999;
+            int expectedResult = Convert.ToInt32((bil3 * 0.85) * 0.2);
+
+            // Act
+            int result = Skat.Afgift.ElBilAfgift(bil3);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void ElCarTax_CarValueIsTwoHundredThousand_TestSucced()
+        {
+            // Arrange
+            int bil4 = 200000;
+            int expectedResult = Convert.ToInt32((bil4 * 0.85) * 0.2);
+
+            // Act
+            int result = Skat.Afgift.ElBilAfgift(bil4);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void ElCarTax_ElCarPriceOverTwoHundredThousand_TestSucced()
+        {
+            // Arrange
+            int bil5 = 250000;
+            int expectedResult = Convert.ToInt32(((bil5 * 1.5) - 130000) * 0.2);
+
+            // Act
+            int result = Skat.Afgift.ElBilAfgift(bil5);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
